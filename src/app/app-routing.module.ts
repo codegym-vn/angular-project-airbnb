174ba2fe0595a5cf44baec45ebdfe2b3c75112e9
LinhC0918 <nguyenvanlinh12d3@gmail.com>
2019-01-11 16:15:58 +0700

    commit 1

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,16 @@
+import {NgModule} from '@angular/core';
+import {Routes, RouterModule} from '@angular/router';
+import {CustomerComponent} from './customer/customer.component';
+import {ViewDetailComponent} from './view-detail/view-detail.component';
+
+const routes: Routes = [
+    {path: 'house', component: CustomerComponent},
+    {path: 'house/detail{id}', component: ViewDetailComponent}
+];
+
+@NgModule({
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule]
+})
+export class AppRoutingModule {
+}
