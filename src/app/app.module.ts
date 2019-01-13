b9eae040d652dd6d75cd799b220e083b4404f6b3
Alexnguyen17 <Alexnguyen170395@gmail.com>
2019-01-14 00:27:06 +0700

    search-home

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import {CustomerComponent} from './customer/customer.component';
 import { ViewDetailComponent } from './view-detail/view-detail.component';
 import { AppRoutingModule } from './/app-routing.module';
 import { BookingHome3Component } from './booking-home3/booking-home3.component';
+import { SearchHomeComponent } from './search-home/search-home.component';
 
 @NgModule({
     declarations: [
         AppComponent,
         CustomerComponent,
         ViewDetailComponent,
-        BookingHome3Component
+        BookingHome3Component,
+        SearchHomeComponent
     ],
     imports: [
         BrowserModule,
