9d6da916a1f9e73befc054edb1937877a6439544
Alexnguyen17 <Alexnguyen170395@gmail.com>
2019-01-16 16:16:56 +0700

    profile

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import {CustomerComponent} from './customer/customer.component';
 import { ViewDetailComponent } from './view-detail/view-detail.component';
 import { AppRoutingModule } from './/app-routing.module';
 import { BookingHome3Component } from './booking-home3/booking-home3.component';
+import { ProfileComponent } from './profile/profile.component';
 
 @NgModule({
     declarations: [
         AppComponent,
         CustomerComponent,
         ViewDetailComponent,
-        BookingHome3Component
+        BookingHome3Component,
+        ProfileComponent
     ],
     imports: [
         BrowserModule,
